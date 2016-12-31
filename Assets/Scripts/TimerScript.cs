a72e8b89426d2f851ef8d5e9155e247be9517277
Anthony Teixeira <anthony.teixeira@ensc.fr>
2016-12-31 18:00:05 +0100

    Ajout Timer + Menu Pause
    wallah

---
+++ Assets/Scripts/TimerScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerScript : MonoBehaviour {
+
+	public Text timerText;
+	private float startTime;
+	// Use this for initialization
+	void Start () {
+		startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float t = Time.time - startTime;
+
+		string minutes = ((int) t / 60).ToString();
+		string minutes2 = (minutes.Length == 1) ? "0" + minutes : minutes;
+		string seconds = (t % 60).ToString("f0");
+		string seconds2 = (seconds.Length == 1) ? "0" + seconds : seconds;
+
+		timerText.text = minutes2 + ":" + seconds2;
+	}
+}
